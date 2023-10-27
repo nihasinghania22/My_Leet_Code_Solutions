026e93d714fda2101ad7cabe0b608484049ad8a5
Niha Singhania <89926534+nihasinghania22@users.noreply.github.com>
2023-10-27 23:05:13 +0530

    Create 217. Contains Duplicate.cpp

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) 
+    {
+
+        bool containsDuplicate(vector<int>& nums);
+        set<int> myset(nums.begin(), nums.end());
+        return myset.size() < nums.size();
+    }
+};
